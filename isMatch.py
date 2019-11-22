0ac51b8bc1f304334e00f97ebf728a26d34f62db
hzy111111 <42538943+hzy111111@users.noreply.github.com>
2019-11-22 09:23:14 +0800

    Update isMatch.py

--- isMatch.py
+++ isMatch.py
@@ -1,4 +1,5 @@
 #coding: utf-8
+import unittest
 """
 字符串匹配，字符串s，字符模式p。
 ‘*’：任意字符串
@@ -6,6 +7,11 @@
 """
 class Solution:
     def isMatch(self, s, p):
+        """
+        :param s:目标字符串s,str
+        :param p:模式字符串,str
+        :return:dp[len(s)][len(p)],是否匹配，bool
+        """
         dp = [[False for j in range(len(p)+1)] for i in range(len(s)+1)]
         dp[0][0] = True
         #填第一行。s=''时，只有'*'和''能匹配
@@ -14,17 +20,71 @@ class Solution:
         #两层循环，填满矩阵，返回最后一个值
         for i in range(1, len(s)+1):
             for j in range(1, len(p)+1):
-                if p[j-1]=='*':
+                if p[j-1] == '*':
                     dp[i][j] = dp[i][j-1] or dp[i-1][j-1] or dp[i-1][j]#只要前面匹配成功，*可以为空或一个任意字符或任意字符
                 else:
-                    dp[i][j]=(p[j-1]=='?' or p[j-1]==s[i-1]) and dp[i-1][j-1]
-        print(dp)
+                    dp[i][j] = (p[j-1] == '?' or p[j-1] == s[i-1]) and dp[i-1][j-1]
+        #print(dp) #打印矩阵
         return dp[len(s)][len(p)]
 
-solve = Solution()
-s="aabc"
-p="a*?"
-print(solve.isMatch(s, p))
+class TestSolution(unittest.TestCase):
+    #出现空的情况
+    def test_empty(self):
+        solve = Solution()
+        s = ""
+        p = ""
+        self.assertEqual(solve.isMatch(s, p), True)
+
+        s = ""
+        p = "*"
+        self.assertEqual(solve.isMatch(s, p), True)
+
+        s = "a"
+        p = ""
+        self.assertEqual(solve.isMatch(s, p), False)
+
+        s = ""
+        p = "?"
+        self.assertEqual(solve.isMatch(s, p), False)
+    #出现*的情况
+    def test_star(self):
+        solve = Solution()
+        s = "abac"
+        p = "*"
+        self.assertEqual(solve.isMatch(s, p), True)
+
+        s = "abac"
+        p = "*b"
+        self.assertEqual(solve.isMatch(s, p), False)
+
+        s = "abac"
+        p = "b*"
+        self.assertEqual(solve.isMatch(s, p), False)
+
+        s = "abac"
+        p = "a*"
+        self.assertEqual(solve.isMatch(s, p), True)
+    #出现？或（？和*）的情况
+    def test_star_que(self):
+        solve = Solution()
+        s = "abac"
+        p = "a?ac"
+        self.assertEqual(solve.isMatch(s, p), True)
+
+        s = "abac"
+        p = "a?c"
+        self.assertEqual(solve.isMatch(s, p), False)
+
+        s = "abac"
+        p = "?*"
+        self.assertEqual(solve.isMatch(s, p), True)
+
+        s = "abac"
+        p = "b?*"
+        self.assertEqual(solve.isMatch(s, p), False)
+
+if __name__=="__main__":
+    unittest.main()
 
 
 
